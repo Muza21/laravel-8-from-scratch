8e6894c782d85cfa88c79b7ef7e4ee71c7bf7679
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-23 22:50:02 +0400

    fix: category names and href links on post cards

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -6,7 +6,7 @@
 <div class="lg:grid lg:grid-cols-6">
     @foreach ($posts->skip(1) as $post)
         {{-- in order to find more information we use: @dd($loop) --}}
-        <x-post-card :post="$posts[1]" class="{{ $loop->iteration<3?'col-span-3':'col-span-2' }}"/>
+        <x-post-card :post="$posts[$loop->iteration]" class="{{ $loop->iteration<3?'col-span-3':'col-span-2' }}"/>
     @endforeach
 </div>
 @endif
