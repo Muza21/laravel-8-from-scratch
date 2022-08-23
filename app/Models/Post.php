7b5022ed36cde393c63ae9b332bdc3f8bea4f658
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-23 22:32:04 +0400

    fix: eloquent query bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,9 +18,9 @@ class Post extends Model
     public function scopeFilter($query, array $fillters) // Post::newQuery()->filter()
     {
         $query->when($fillters['search'] ?? false, fn ($query, $search) =>
-        $query
-            ->where('title', 'like', '%' . $search . '%')
-            ->orWhere('body', 'like', '%' . $search . '%'));
+        $query->where(fn ($query) =>
+        $query->where('title', 'like', '%' . $search . '%')
+            ->orWhere('body', 'like', '%' . $search . '%')));
 
         $query->when(
             $fillters['category'] ?? false,
