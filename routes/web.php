1965a86dd3a1b8f11eb397f5fcfea8ab5c79a762
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-24 13:02:06 +0400

    feat: build a register user page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 // use phpDocumentor\Reflection\Types\Collection;
 // use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -24,6 +25,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
+
 
 // Route::get('categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
