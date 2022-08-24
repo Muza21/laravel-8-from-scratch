351fbf68401a3f40d1ed3c747d445ae3e9a2aa4b
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-24 19:06:22 +0400

    feat: add log in page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+
+    public function create(){
+        return view('sessions.create');
+    }
+
+    public function store(){
+        $attributes = request()->validate([
+            'email'=> 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($attributes)){
+            return redirect('/')->with('success','Welcome Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email'=>'Your provided credentials could not be identified.'
+        ]);
+
+        // auth failed.
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email'=>'Your provided credentials could not be identified.']);
+    }
+
     public function destroy(){
         auth()->logout();
 
