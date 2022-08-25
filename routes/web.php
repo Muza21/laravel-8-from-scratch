e5a9126d5a38a3fc8442701f24c6d80d133a4c27
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-25 14:58:01 +0400

    feat: add subscribe

--- routes/web.php
+++ routes/web.php
@@ -25,8 +25,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('ping',function(){
-    
+Route::post('/newsletter', function(){
+    request()->validate(['email' => 'required|email']);
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -34,12 +34,20 @@ Route::get('ping',function(){
         'server' => 'us13'
     ]);
 
-    $response = $mailchimp->lists->addListMember('61d50479a3', [
-        'email_address' => 'defaultmail@gmail.com',
-        'status' => 'subscribed'
-    ]);
+    try{
+        $response = $mailchimp->lists->addListMember('61d50479a3', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    }
+    catch(\Exception $e){
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+    
 
-    ddd($response);
+    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
