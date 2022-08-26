c511dc3d6ed6705f14352af368e2e2c472463e4e
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-26 11:54:23 +0400

    feat: add admin route

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,11 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 
 Route::get('login', [SessionsController::class,'create'])->middleware('guest');
 Route::post('login', [SessionsController::class,'store'])->middleware('guest');
+
 Route::post('logout', [SessionsController::class,'destroy'])->middleware('auth');
+
+Route::get('admin/posts/create', [PostController::class,'create'])->middleware('admin');
+
 
 // Route::get('categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
