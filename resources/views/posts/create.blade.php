8792a6a0f90f67e2431f585a8023fe738acb5c90
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-26 17:48:57 +0400

    refactor: extract form-specific blade components

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,97 +6,17 @@
         <x-panel>
             <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="title">
-                        Title
-                    </lable>
-        
-                    <input type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="title"
-                        id="title"
-                        value="{{ old('title') }}"
-                        required
-                    >
-        
-                    @error('title')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="slug">
-                        Slug
-                    </lable>
-        
-                    <input type="text"
-                        class="border border-gray-400 p-2 w-full"
-                        name="slug"
-                        id="slug"
-                        value="{{ old('slug') }}"
-                        required
-                    >
-        
-                    @error('slug')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="thumbnail">
-                        Thumbnail
-                    </lable>
-        
-                    <input type="file"
-                        class="border border-gray-400 p-2 w-full"
-                        name="thumbnail"
-                        id="thumbnail"
-                        required
-                    >
+                
+                <x-form.input name="title"/>
+                <x-form.input name="slug"/>
+                <x-form.input name="thumbnail" type="file" />
 
-                    @error('thumbnail')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="excerpt">
-                        Excerpt
-                    </lable>
-        
-                    <textarea class="border border-gray-400 p-2 w-full"
-                            name="excerpt"
-                            id="excerpt"
-                            required
-                    >{{ old('excerpt') }}</textarea>
-        
-                    @error('excerpt')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="body">
-                        Body
-                    </lable>
-        
-                    <textarea class="border border-gray-400 p-2 w-full"
-                            name="body"
-                            id="body"
-                            required
-                    >{{ old('body') }}</textarea>
-        
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <div class="mb-6">
-                    <lable class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                        for="category">
-                        Category
-                    </lable>
-        
+                <x-form.textarea name="excerpt" />
+                <x-form.textarea name="body" />
+
+                <x-form.field>
+                    <x-form.label name="category" />
+
                     <select name="category_id" id="category_id">
                         @foreach (App\Models\Category::all() as $category)
                             <option value="{{ $category->id }}" 
@@ -107,12 +27,9 @@
                         @endforeach
                         
                     </select>
-        
-                    @error('category_id')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-                <x-submit-button>Publish</x-submit-button>
+                    <x-form.error name="category"/>
+                </x-form.field>
+                <x-form.button>Publish</x-form.button>
             </form>
         </x-panel>
     </section>
