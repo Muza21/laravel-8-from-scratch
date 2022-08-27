b4b79f3677e7759427ce3c0865201b7f23a7aefe
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-08-27 15:35:54 +0400

    fix: publish and update bug

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -9,8 +9,9 @@ use Psy\Command\EditCommand;
 
 class AdminPostController extends Controller
 {
-    public function index(){
-        return view('admin.posts.index',[
+    public function index()
+    {
+        return view('admin.posts.index', [
             'posts' => Post::paginate(50)
         ]);
     }
@@ -22,7 +23,7 @@ class AdminPostController extends Controller
 
     public function store()
     {
-        Post::create(array_merge($this->validatePost(),[
+        Post::create(array_merge($this->validatePost(), [
             'user_id' => request()->user()->id,
             'thumbnail' => request()->file('thumbnail')->store('thumbnails')
         ]));
@@ -30,40 +31,42 @@ class AdminPostController extends Controller
         return redirect('/');
     }
 
-    public function edit(Post $post){
-        return view('admin.posts.edit',['post' => $post]);
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', ['post' => $post]);
     }
 
-    public function update(Post $post){
+    public function update(Post $post)
+    {
         $attributes = $this->validatePost($post);
 
-        if($attributes['thumbnail'] ?? false){
+        if ($attributes['thumbnail'] ?? false) {
             $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
         }
-       
 
         $post->update($attributes);
 
-        return back()->with('success','Post Updated!');
+        return back()->with('success', 'Post Updated!');
     }
 
-    public function destroy(Post $post){
+    public function destroy(Post $post)
+    {
         $post->delete();
 
-        return back()->with('success','Post Deleted!');
+        return back()->with('success', 'Post Deleted!');
     }
 
-    protected function validatePost(?Post $post = null):array{
+    protected function validatePost(?Post $post = null): array
+    {
         $post ??= new Post();
 
         return request()->validate([
             'title' => 'required',
-            'thumbnail' => $post->exists ? ['image'] :['required','image'],
-            'slug' => ['required',Rule::unique('posts','slug')->ignore($post->id)],
+            'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post)],
             'excerpt' => 'required',
             'body' => 'required',
-            'category_id' => ['required',Rule::exists('categories','id')],
-            'published_at' => 'required'
+            'category_id' => ['required', Rule::exists('categories', 'id')]
         ]);
     }
 }
